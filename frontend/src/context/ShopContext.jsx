d8e85d8f6bc1b55f1ed7478c965d47da51396f18
Aditya20097 <raj.aditya10997@gmail.com>
2025-10-04 18:49:21 +0530

    backend

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -134,7 +134,8 @@ const ShopContextProvider = (props)=>{
 
         const getProductData = async () => {
             try {
-                const response = await axios.get('http://localhost:4000/api/product/list')
+                const response =axios.get("https://klicksy-backend.vercel.app/api/product/list")
+
                 if(response.data.success) {
                     setProduct(response.data.products)
                 } else {
