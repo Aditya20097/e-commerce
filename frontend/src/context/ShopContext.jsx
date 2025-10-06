50f0c0fb62743b6499c4b0e0a9ada03c05f9db28
Aditya20097 <raj.aditya10997@gmail.com>
2025-10-06 15:29:25 +0530

    axios update

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -134,7 +134,7 @@ const ShopContextProvider = (props)=>{
 
         const getProductData = async () => {
             try {
-                const response = await axios.get("https://klicksy-backend.vercel.app/api/product/list")
+                const response = await axios.get("backendUrl + api/product/list")
 
                 if(response.data.success) {
                     setProduct(response.data.products)
